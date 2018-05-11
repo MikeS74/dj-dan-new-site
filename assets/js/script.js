c6e7ddc20e105256414b682c755bd75d40743ca5
MikeS74 <futurefreq@hotmail.com>
2018-05-10 20:16:01 -0700

    bio page fade working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-$( document ).ready(function() {    
-$(".home-container").hide().delay("slow").fadeIn(2000);    
+$(document).ready(function () {
+
+    //ORIG FADE IN
+    $("#bio-pg").hide();
+    $(".home-container").hide().delay("slow").fadeIn(2000);
+
+    //ON CLICKS
+    $("#bio-link").on("click", function () {
+        $("#bio-pg").delay("slow").fadeIn(500);
+        $(".home-container").fadeOut(500);
+    });
+
+    $(".close-button").on("click", function () {
+        $("#bio-pg").fadeOut(500);
+        $(".home-container").delay("slow").fadeIn(500);
+    });
 });
